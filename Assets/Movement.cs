53e77a812761e6767d1ef636da156c1a51314987
Lukas Tutkus <Lukas Tutkus>
2017-01-21 09:48:17 +0200

    Adding multiplaya test scene, head img and prefab, movement script

---
+++ Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Movement : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if(!isLocalPlayer)
+        {
+            Destroy(this);
+            return;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float translation = CrossPlatformInputManager.GetAxis("Horizontal") * 5 * Time.deltaTime;
+        transform.Translate(translation, 0, 0);
+    }
+
+
+}
