c9c032226e05fb55093cbd140e351e79d68306af
Lukas Tutkus <Lukas Tutkus>
2017-01-21 13:36:14 +0200

    Added text fields for network names added global variables.

---
+++ Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalVariables
+{
+    public static int playerCount = 0;
+    public static string playerName = "";
+    public static bool isHostOn = false;
+    public static bool isClientOn = false;
+}
---
+++ Assets/Scripts/SetupName.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupName : NetworkBehaviour {
+    public TextMesh tm;
+
+
+	// Use this for initialization
+	void Start () {
+		if(!isLocalPlayer)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Debug.Log(GlobalVariables.playerCount);
+        Debug.Log(GlobalVariables.playerName);
+        if (GlobalVariables.playerName == "")
+        {
+            if (GlobalVariables.playerCount % 2 == 1)
+                tm.text = "Player B";
+            else
+                tm.text = "Player A";
+
+        }
+        else
+        {
+            if(tm.text == "")
+                tm.text = GlobalVariables.playerName;
+        }        
+        GlobalVariables.playerCount++;
+	}
+	
+}
