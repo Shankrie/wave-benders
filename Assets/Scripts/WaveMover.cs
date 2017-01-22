7cb9825f182ec34e4a8850f0b922660bcc2d1294
Lukas Tutkus <Lukas Tutkus>
2017-01-22 12:46:10 +0200

    Wave mover changes. Added public method for deflecting wave.

--- Assets/Scripts/WaveMover.cs
+++ Assets/Scripts/WaveMover.cs
@@ -7,66 +7,88 @@ public class WaveMover : MonoBehaviour {
     public Transform Aposition;
     public Transform Bposition;
 
-    private Vector3 pointB;
-    private Vector3 pointA;
-
     private Vector3 overflowPointA;
-    private Vector3 overflowPointB;                 
+    private Vector3 overflowPointB;
 
-    private float timer = 4.0f;
+    private float maxEndXPoint;
+    private float maxStartXPoint;
+    private float speed = 5.0f;
+    private float time = 10f;
 
     private bool isFacingRight = true;
-    private bool haveReachedEnd = false;
+    private bool notReachedEnd = true;
+    private bool checkReachedEnd = false;
 
     const float INCREASE_SCALE_Y = 0.05f;
     const float INCREASE_SCALE_X = 0.025f;
     const float DECREASE_TIMER_VALUE = 0.1f;
     const float MAX_SCALE_Y = 1.0f;
     const float MIN_TIMER_VALUE = 1.0f;
-
-    IEnumerator Start()
-    {
-        
-        transform.localScale = new Vector3(transform.localScale.x, 0.2f, transform.localScale.z);
-        pointA = Aposition.position;
-        pointB = Bposition.position;
-        overflowPointA = pointA + new Vector3(3, 0, 0);
-        Vector3 temp;
-        while (true)    
-        {
-            yield return StartCoroutine(MoveObject(transform, timer));
-            temp = pointA;
-            pointA = pointB;
-            pointB = temp;
-
-        }    
-    }
     
-    void FixedUpdate()
+
+    void Start()
     {
-        
+        transform.localScale = new Vector3(transform.localScale.x, 0.2f, transform.localScale.z);
+        overflowPointA = Aposition.position + new Vector3(-5, 0, 0);
+        overflowPointB = Bposition.position + new Vector3(5, 0, 0);
+        maxStartXPoint = Aposition.position.x;
+        maxEndXPoint = Bposition.position.x;
+        MoveOverSpeed();
+        StartCoroutine(MoveOverSeconds());
     }
 
-    IEnumerator MoveObject(Transform thisTransform, float time)
+    public IEnumerator MoveOverSpeed()
     {
-        var i = 0.0f;
-        var rate = 1.0f / time;
-        while (i < 1.0f)
+        // speed should be 1 unit per second
+        while (true)
         {
-            i += Time.deltaTime * rate;
-                
-            if (thisTransform.position == pointB || thisTransform.position == pointA)
-            {
-                Flipper();
-                ScaleAndLiftWave();
-                SpeedIncreaser();
-                PlayWaveRising();
-            }
-            thisTransform.position = Vector3.Lerp(pointA, pointB, i);
-            yield return null;
+            transform.position = Vector3.MoveTowards(transform.position, overflowPointB, speed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
         }
     }
 
+    public IEnumerator MoveOverSeconds()
+    {
+        float elapsedTime = 0;
+        Vector3 startingPos = transform.position;
+        while (elapsedTime < time)
+        {
+            transform.position = Vector3.Lerp(transform.position, overflowPointB, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        //transform.position = end;
+    }
+
+    //void Update()
+    //{
+    //    if (checkReachedEnd == false && (transform.position.x <= maxStartXPoint || transform.position.x >= maxEndXPoint))
+    //    {
+    //        checkReachedEnd = true;
+    //    }
+    //    else if (checkReachedEnd == true && (Mathf.Abs(transform.position.x) >= Mathf.Abs(overflowPointB.x) - 0.1f))
+    //    {
+    //        notReachedEnd = false;
+    //    }
+
+
+
+    //    //if (Input.GetKeyDown(KeyCode.A))
+    //    //{
+    //    //    Flipper();
+    //    //    //ScaleAndLiftWave();
+    //    //    //SpeedIncreaser();
+    //    //    //PlayWaveRising();
+    //    //    GlobalVariables.sendWaveAway = false;
+    //    //    Vector3 temp = overflowPointA;
+    //    //    overflowPointA = overflowPointB;
+    //    //    overflowPointB = temp;
+    //    //    yield return null;
+    //    //}
+
+
+    //}
+   
     void Flipper()
     {
         isFacingRight = !isFacingRight;
@@ -81,17 +103,10 @@ public class WaveMover : MonoBehaviour {
             return;
 
         transform.localScale += new Vector3(INCREASE_SCALE_X * Mathf.Sign(transform.localScale.x), INCREASE_SCALE_Y, 0);
-        pointA += new Vector3(0, INCREASE_SCALE_Y * 5, 0);
-        pointB += new Vector3(0, INCREASE_SCALE_Y * 5, 0);
+        overflowPointA += new Vector3(0, INCREASE_SCALE_Y * 5, 0);
+        overflowPointB += new Vector3(0, INCREASE_SCALE_Y * 5, 0);
     }
 
-    void SpeedIncreaser()
-    {
-        if(timer >= MIN_TIMER_VALUE)
-        {
-            timer -= DECREASE_TIMER_VALUE;
-        }
-    }
 
     void PlayWaveRising()
     {
@@ -99,4 +114,16 @@ public class WaveMover : MonoBehaviour {
         audioSource.clip = Resources.Load("waveRising") as AudioClip;
         audioSource.Play();
     }
+
+    public void deflectWave()
+    {
+        Flipper();
+        ScaleAndLiftWave();
+        PlayWaveRising();
+        GlobalVariables.sendWaveAway = false;
+        Vector3 temp = overflowPointA;
+        overflowPointA = overflowPointB;
+        overflowPointB = temp;
+    }
+
 }
