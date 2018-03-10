79cf95f66608e0b877eca6cb9676156a87414496
Lukas Tutkus <lt.tutkus7@gmail.com>
2018-03-10 15:04:22 +0200

    TAHL.WaveBenders. Add script which fixes transform position

---
+++ Assets/Scripts/FixedRect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Small script fixing rect transform height and width
+/// </summary>
+public class FixedRect : MonoBehaviour {
+
+    public Vector2 fixedPosition = Vector2.zero;
+    public Vector2 fixedSize = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        RectTransform rect = GetComponent<RectTransform>();
+        if (rect == null)
+            throw new Exception("Rect Transform not found in gameobject");
+        rect.anchoredPosition = fixedPosition;
+        rect.sizeDelta = fixedSize;
+	}
+
+}
