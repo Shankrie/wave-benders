65581de675689194fe45344d4bf052e0ae1852b4
Lukas Tutkus <lt.tutkus7@gmail.com>
2018-03-10 15:01:14 +0200

    TAHL. WaveBenders. Rename Search field

--- Assets/Scripts/ShowSuggestions.cs
+++ Assets/Scripts/ShowSuggestions.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace TAHL.WAVE_BENDER
 {
+    /// <summary>
+    /// Basic concept of ShowSuggestions is to show dropdown of suggestions when input is filled up
+    /// </summary>
     [RequireComponent(typeof(Dropdown))]
-    class SearchField: MonoBehaviour
+    class ShowSuggestions: MonoBehaviour
     {
+        #region variables
+
         public FocusInputField RoomNameInput = null;
 
         private List<string> _filteredOptions = new List<string>();
@@ -21,82 +23,102 @@ namespace TAHL.WAVE_BENDER
 
         private bool _hidden = true;
         private bool _ignoreSelected = true;
-
         private bool _first = true;
 
-        void Start()
+        #endregion
+
+        #region Private methods
+
+        private void Start()
         {
             if(RoomNameInput == null)
             {
                 enabled = false;
                 throw new Exception("Room Name Input Field is required component");
             }
+
             _allRooms = GetComponent<Dropdown>();
             _allRooms.onValueChanged.AddListener(OnSelect);
         }
 
-        private void Update()
-        {
-            if(_text != RoomNameInput.text)
-            {
-                _text = RoomNameInput.text;
-
-                // Ignore first time changes from other scripts like PlayerPrefLoader
-                if(_first)
-                {
-                    _first = false;
-                    return;
-                }
-
-                UpdateDropdownList();
-
-                // Again reselect input field
-                RoomNameInput.Select();
-            }
-        }
-
-        private void UpdateDropdownList()
+        private void UpdateSuggestions()
         {
             if(_text.Length > 2)
             {
-                _allRooms.interactable = true;
-
+                // clear left options
                 _allRooms.ClearOptions();
                 _filteredOptions.Clear();
+
+                // filter room list by current roomInfo name
                 foreach (RoomInfo roomInfo in PhotonNetwork.GetRoomList()) {
                     if(Regex.Match(roomInfo.Name, _text, RegexOptions.IgnoreCase).Success)
                     {
                         _filteredOptions.Add(roomInfo.Name);
                     }
                 }
+
+                // if no found then 
+                if (_filteredOptions.Count == 0)
+                {
+                    if(!_hidden)
+                        ShowDropdown(false);
+                    return;
+                }
+
+                // Set interactable and add filtered options
+                _ignoreSelected = true;
+                _allRooms.interactable = true;
                 _allRooms.AddOptions(_filteredOptions);
 
-                // Add empty option, select it and remove it so there won't be any selected options
-                _ignoreSelected = true;
+                // Remove Checkbox by setting and removing dropdown selected item
                 _allRooms.options.Add(new Dropdown.OptionData() { text = "" });
                 _allRooms.value = _allRooms.options.Count - 1;
                 _allRooms.options.RemoveAt(_allRooms.options.Count - 1);
 
                 if(_hidden)
                 {
-                    // don't ya loose focus of input
-                    RoomNameInput.ForceFocus = true;
-
-                    _allRooms.Show();
-                    _hidden = false;
+                    ShowDropdown(true);
                 }
             }
             else if(!_hidden)
             {
-                // set dropdown to non interactable 
-                _allRooms.interactable = false;
-
-                // don't ya loose focus of input
-                RoomNameInput.ForceFocus = true;
-
-                _allRooms.Hide();
-                _hidden = true;
+                ShowDropdown(false);
             }
+        }
+
+        private void ShowDropdown(bool show)
+        {
+            // set dropdown to non interactable 
+            _allRooms.interactable = show;
+
+            // don't ya loose focus of input
+            RoomNameInput.ForceFocus = true;
+
+            if (show)
+                _allRooms.Show();
+            else
+                _allRooms.Hide();
+
+            _hidden = !show;
+        }
+
+        #endregion
+
+        #region Value Change And Select Event methods
+
+        public void OnTextValueChange(string text)
+        {
+            // Ignore first time changes from other scripts like PlayerPrefLoader
+            if (_first)
+            {
+                _first = false;
+                return;
+            }
+            _text = text;
+            UpdateSuggestions();
+
+            // Again reselect input field
+            RoomNameInput.Select();
         }
 
         public void OnSelect(int index)
@@ -113,5 +135,7 @@ namespace TAHL.WAVE_BENDER
             RoomNameInput.text = text;
             RoomNameInput.Select();
         }
+
+        #endregion
     }
 }
