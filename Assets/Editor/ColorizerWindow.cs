5a448392cd6f09f7cc2f196fff03e044db9a7885
Lukas Tutkus <lt.tutkus7@gmail.com>
2018-03-10 15:05:36 +0200

    TAHL.WaveBenders. Testing editor window capabilities.

---
+++ Assets/Editor/ColorizerWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorizerWindow : EditorWindow
+{
+    Color color = Color.white;
+
+    [MenuItem("Window/Colorizer")]
+    public static void ShowWindow()
+    {
+        GetWindow<ColorizerWindow>("Colorizer");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Change color of GUIs backgrounds ", EditorStyles.boldLabel);
+        color = EditorGUILayout.ColorField("Color", color);
+
+        if(GUILayout.Button("Colorize GUIs"))
+        {
+            foreach(GameObject background in GameObject.FindGameObjectsWithTag("GUIBackground"))
+            {
+                background.GetComponent<Image>().color = color;
+            }
+        }
+    }
+}
---
+++ Assets/Editor/CustomInspect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+[CustomEditor(typeof(Canvas))]
+public class CustomInspect: Editor
+{
+    Color color;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+
+        color = EditorGUILayout.ColorField("Color", color);
+        if(GUILayout.Button("Apply Color"))
+        {
+            foreach (GameObject go in GameObject.FindGameObjectsWithTag("GUIBackground"))
+            {
+                go.GetComponent<Image>().color = color;
+            }
+        }
+
+    }
+
+}
