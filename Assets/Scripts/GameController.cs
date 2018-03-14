c9e1268673ec6d6b830fa024d2642cc0cd229a31
Lukas Tutkus <lt.tutkus7@gmail.com>
2018-03-14 21:36:54 +0200

    TAHL.WaveBenders. Move Countdown code to GameController.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,32 +2,59 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Photon;
 
 namespace TAHL.WAVE_BENDER
 {
-    public class GameController : MonoBehaviour
+    public class GameController : PunBehaviour
     {
-        // Initial Master Client Position
-        public Transform MasterClientPosition;
-        // Initial Client Position
-        public Transform ClientPosition;
+        public GameObject MasterClient = null;
+        public GameObject Client = null;
+
+        private KeyController _mineKeyController = null;
+
+        private GameObject[] _countDownObjs;
+
         // Winner label in Game End GUI
         public Text WinnerLabel;
         public Text LoseCause;
 
-        private GameObject _masterClientObject;
-        private GameObject _clientObject;
         private GameObject _gameEndGUI;
         private GameObject _networkGUI;
 
-        private KeyController _masterClientKeyController;
-        private KeyController _clientKeyController;
+        private float _countDownTime = 0;
+        private int _index = 0;
 
+        private bool _startCountDown = false;
 
         private void Awake()
         {
-            _masterClientObject = null;
-            _clientObject = null;
+            if (MasterClient == null)
+            {
+                throw new Exception("Error. Master client not supplied");
+            }
+            if (Client == null)
+            {
+                throw new Exception("Error. Client not supplied");
+            }
+
+            // Get Countdown objects and disable em
+            Globals.Methods.SetupGOs(ref _countDownObjs, Globals.Names.CountDowns, Globals.Tags.CountDownObjs);
+            foreach(GameObject go in _countDownObjs)
+            {
+                go.SetActive(false);
+            }
+
+            if(PhotonNetwork.isMasterClient)
+            {
+                MasterClient.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.player);
+                _mineKeyController = MasterClient.GetComponent<KeyController>();
+            }
+            else
+            {
+                Client.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.player);
+                _mineKeyController = Client.GetComponent<KeyController>();
+            }
 
             _gameEndGUI = GameObject.FindGameObjectWithTag(Globals.Tags.GameEndGUI);
             _gameEndGUI.SetActive(false);
@@ -42,6 +69,29 @@ namespace TAHL.WAVE_BENDER
             {
                 _gameEndGUI.SetActive(!_gameEndGUI.activeSelf);
             }
+            if (_startCountDown && _countDownTime - 0.01 > Globals.Delays.COUNT_DOWN)
+            {
+                _countDownObjs[_index++].SetActive(false);
+                if (_index >= _countDownObjs.Length)
+                {
+                    _startCountDown = false;
+                    _mineKeyController.InitializeCall();
+                }
+                else
+                {
+                    _countDownObjs[_index].SetActive(true);
+                }
+                _countDownTime = 0;
+            }
+            else if (_startCountDown)
+            {
+                _countDownTime += Time.fixedDeltaTime;
+            }
+        }
+
+        private void OnGUI()
+        {
+            GUI.TextArea(new Rect(0, 0, 100, 100), _mineKeyController.gameObject.name);
         }
 
         public void EnableGameEndGUI(bool enable)
@@ -63,7 +113,7 @@ namespace TAHL.WAVE_BENDER
         public void GotoMainMenu()
         {
             PhotonNetwork.Disconnect();
-            SceneManager.LoadScene((int)Globals.SceneIndex.MainMenu);
+            SceneManager.LoadScene((int)Globals.Enums.SceneIndex.MainMenu);
         }
 
         /// <summary>
@@ -73,27 +123,11 @@ namespace TAHL.WAVE_BENDER
         {
             try
             {
-                KeyController mineKeyController = null;
-                GameObject[] players = GameObject.FindGameObjectsWithTag(Globals.Tags.Player);
-                foreach (GameObject player in players)
+                if (_mineKeyController == null)
                 {
-                    KeyController keyController = player.GetComponent<KeyController>();
-                    if(mineKeyController == null && keyController.ControlsView)
-                    {
-                        mineKeyController = keyController;
-                    }
-                    if (player.GetComponent<KeyController>().PlayerDirection == 1)
-                    {
-                        _masterClientObject = player;
-                        _masterClientKeyController = player.GetComponent<KeyController>();
-                    }
-                    else
-                    {
-                        _clientObject = player;
-                        _clientKeyController = player.GetComponent<KeyController>();
-                    }
+                    throw new Exception("Error. Mine key controller is not defined");
                 }
-                mineKeyController.ResetLevelCall();
+                _mineKeyController.ResetLevelCall();
             }
             catch (Exception ex)
             {
@@ -101,11 +135,28 @@ namespace TAHL.WAVE_BENDER
             }
         }
 
-
         public void ReturnToLobby()
         {
             PhotonNetwork.LeaveRoom();
-            SceneManager.LoadScene((int)Globals.SceneIndex.Lobby);
+            SceneManager.LoadScene((int)Globals.Enums.SceneIndex.Lobby);
+        }
+
+        public void StartCountDown()
+        {
+            _countDownObjs[0].SetActive(false);
+            _index = 0;
+            _countDownTime = 0;
+            _startCountDown = true;
+            _countDownObjs[_index].SetActive(true);
+        }
+
+        public override void OnOwnershipTransfered(object[] viewAndPlayers)
+        {
+            Debug.Log("Test32123");
+            if(PhotonNetwork.isMasterClient)
+            {
+                // _mineKeyController.StartCountDownCall();
+            }
         }
     }
 }
